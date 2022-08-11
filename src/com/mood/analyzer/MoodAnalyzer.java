1c3ee2b6d381c3a3b040a32e1dc09c27d6bb0cd9
Umeshkmaurya197 <umeshkumarmaurya.98@gmail.com>
2022-08-11 14:45:02 +0530

    Refactored : main() Method  in MoodAnalyzer.java

--- src/com/mood/analyzer/MoodAnalyzer.java
+++ src/com/mood/analyzer/MoodAnalyzer.java
@@ -3,9 +3,10 @@ package com.mood.analyzer;
 import java.util.Scanner;
 
 public class MoodAnalyzer {
-	String massage;
+	String massage = null;
 
-	public MoodAnalyzer() {}
+	public MoodAnalyzer() {
+	}
 
 	public MoodAnalyzer(String massage) {
 		this.massage = massage;
@@ -20,14 +21,24 @@ public class MoodAnalyzer {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter mood : ");
-		String msg = scanner.nextLine();
+		try {
+			System.out.print("Enter mood : ");
+			String msg = scanner.nextLine();
+			if (msg != "" || msg.equalsIgnoreCase("Happy") || msg.equals("Sad")) {
+				MoodAnalyzer moodAnalyzer = new MoodAnalyzer(msg);
+				String mood = moodAnalyzer.analyseMood();
+				System.out.println("Current Mood is :" + mood);
+			} else {
+				extracted();
+			}
 
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer(msg);
-
-		String mood = moodAnalyzer.analyseMood();
-		System.out.println("Current Mood is :" + mood);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 		scanner.close();
 	}
 
+	private static void extracted() {
+		throw new NullPointerException();
+	}
 }
